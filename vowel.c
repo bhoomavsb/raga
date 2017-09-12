989d2521dbb9f339a0e69ea0ba50ee189068ba1a
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2017-09-12 15:48:58 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,27 @@
+# raga
+#include<stdio.h>
+voidmain()
+{
+char letter;
+printf("enter the letter");
+scanf("%c",&letter);
+switch(letter)
+case a:
+printf("vowel");
+break;
+case e:
+printf("vowel");
+break;
+case i:
+printf("vowel");
+break;
+case o:
+printf("vowel");
+break;
+case u:
+printf("vowel");
+break;
+default:
+printf("consonant");
+break;
+}
